f0f10cc77493906da29ce3bb250e69da68c9c2dc
fo95116 <fo95116@gmail.com>
2014-06-09 02:43:16 -0700

    almost there

--- scripts/script.js
+++ scripts/script.js
@@ -7,43 +7,53 @@ $(function() {
       top = $('#dials').offset().top,
       redX = $('#red').offset().left,
       greenX = $('#green').offset().left,
-      blueX = $('#blue').offset().left;
+      blueX = $('#blue').offset().left,
+      data = [],
+      rgb = 'rgb(0,0,0)';
 
+  // new functionality to get color in single event
+  // everytime this event is fired it must update the functionality
+  $('#dials').on('click', function update_color(event) {
+    event.stopPropagation();
+    // get the postion of the click event
+    var posX = event.pageX;
+    var posY = Math.round(event.pageY - top);
 
+    // determine whether the action corresponds to r, g, or b
+    if(posX > redX && posX < greenX){
+      console.log('this is red');
+      var r = posY;
+      data.push(r);
+    }
+    if(posX > greenX && posX < blueX){
+      console.log('this is green');
+      var g = posY;
+      data.push(g);
+    }
+    if(posX > blueX ){
+      console.log('this is blue');
+      var b = posY;
+      data.push(b);
+    }
 
+    console.log(data);
 
-// new functionality to get color in single event
-// everytime this event is fired it must update the functionality
-$('#dials').on('click', function update_color(event) {
-  event.stopPropagation();
-  // get the postion of the click event
-  var posX = event.pageX;
-  var posY = Math.round(event.pageY - top);
-  var data = [];
+    // keep track of the values that are being fired
+    if(data[data.length - 1] == r && (posX > redX && posX < greenX)){
+      rVal = data.pop();
+      console.log('red space ' + 'and pop is ' + rVal);
+    }
+    if(data[data.length - 1] == g && (posX > greenX && posX < blueX)){
+      gVal = data.pop();
+      console.log('green space  ' + 'and pop is '+ gVal);
+    }
+    if(data[data.length - 1] == b && (posX > blueX)){
+      bVal = data.pop();
+      console.log('blue space ' + 'and pop is '+ bVal);
+    }
 
-  // determine whether the action corresponds to r, g, or b
-  if(posX > redX && posX < greenX){
-    console.log('this is red');
-    var r = posY;
-
-  }
-  if(posX > greenX && posX < blueX){
-    console.log('this is green');
-    var g = posY;
-
-  }
-  if(posX > blueX ){
-    console.log('this is blue');
-    var b = posY;
-
-  }
-  data = [r,g,b];
-  console.log(data);
-
-})
-
-    //var data = (first,second,third).data
-    //console.log(data)
+    $('#color').css('background-color', 'rgb(' + rVal + ',' + '0' + ',' + '0' + ')');
+  })
 
 
 })
